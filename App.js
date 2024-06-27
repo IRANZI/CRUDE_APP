95da699f336572b04bf26d3cab9d84a05bcf9b29
IRANZI <154799967+IRANZI@users.noreply.github.com>
2024-06-27 17:04:53 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Sidebar from "./components/sidebar";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+// import Home from "./components/home";
+import Render from "./components/render";
+
+
+
+function App() {
+  
+  return (
+    <div className="container-fluid bg-secondary min-vh-100">
+      <div className="row">
+       
+        
+      </div>
+      <Render/>
+
+    
+    </div>
+
+   
+  );
+}
+
+export default App;
